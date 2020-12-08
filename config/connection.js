a658873fedb253424ddaa1808bbfd41959ae752a
Amee Parmar <amee05@yahoo.com>
2020-12-07 16:42:31 -0800

    getting database connection

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,9 @@
 const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:rootroot@localhost/users_db')
+const connection = mysql.createConnection(process.env.JAWSDB_URL || `{
+  host: 'localhost',
+  user: 'root',
+  password: '#eemaMi05',
+  database: 'employee_tracker'
+}`)
 
 module.exports = connection
