c5c978e365006755dae6da6ace33488fd40ea6d2
Amee Parmar <amee05@yahoo.com>
2020-12-07 21:15:25 -0800

    fixing again

--- public/assets/app.js
+++ public/assets/app.js
@@ -4,15 +4,15 @@ document.getElementById('addBurger').addEventListener('click', event => {
   event.preventDefault()
   axios.post('/api/burgers', {
     name: document.getElementById(burger).value,
-    isHere: false
+    devoured: false
   })
     .then(() => location.reload())
     .catch(err => console.error(err))
 })
 
 document.addEventListener('click', event => {
-  if (event.target.className === 'isHere') {
-    axios.put(`/api/burgers/${event.target.dataset.id}`, { isHere: true })
+  if (event.target.className === 'devoured') {
+    axios.put(`/api/burgers/${event.target.dataset.id}`, { devoured: true })
     .then(() => location.reload())
     .catch(err => console.error(err))
   } else if (event.target.className === 'delete') {
--- sql/schema.sql
+++ sql/schema.sql
@@ -7,5 +7,5 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
-    isHere BOOL NOT NULL
+    devoured BOOL NOT NULL
 );
