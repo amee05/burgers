dc6d1c704a97fa61eeca7eb961e5699872d669a8
Amee Parmar <amee05@yahoo.com>
2020-12-07 21:23:24 -0800

    ugh-typing mistakes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => res.redirect('/api/users'))
-router.use('/api', require('./userController.js'))
+router.use('/api', require('./burgerController.js'))
 
 module.exports = router
